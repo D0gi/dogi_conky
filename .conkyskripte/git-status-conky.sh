55658be1fb935a988eb72cc2b41b28033e4e88a9
d0gi <philipp.dockhorn@gmail.com>
2014-11-13 19:23:02 +0100

    improve git-status (commit & push)

--- .conkyskripte/git-status-conky.sh
+++ .conkyskripte/git-status-conky.sh
@@ -1,3 +1,21 @@
 #!/bin/sh
 cd ~/git/conky
-git status --porcelain | wc -l
+ret1=$(git status --porcelain | wc -l)
+ret2=$(git status | grep "Ihr Branch ist vor .* Commit" | wc -l)
+
+if [ $ret1 -eq "0" ]
+  then
+    ret="c"
+  else
+    ret="0"
+fi
+if [ $ret2 -eq "0" ]
+  then
+    ret="$ret p"
+  else
+    ret="$ret 0"
+fi
+
+
+echo $ret
+
