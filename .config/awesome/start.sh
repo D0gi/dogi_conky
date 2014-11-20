eccf8f442838ffdf2d49b231c7543ebb6feba2fb
Philipp Dockhorn <philipp.dockhorn@gmail.com>
2014-11-20 14:09:04 +0100

    improve start.sh and screenlayout

--- .config/awesome/start.sh
+++ .config/awesome/start.sh
@@ -1,11 +1,18 @@
 r=$(xrandr|grep VGA|grep disconnected)
 if [[ $r == "" ]]; then
-  ~/.screenlayout/rechtermonitor.sh
+  ~/.screenlayout/doppelmonitor.sh
 else
   ~/.screenlayout/laptop.sh
 fi
+date >> ~/zeitstempel
 killall conky
 killall nm-applet
+#btsync kill
+kill $(ps aux | grep btsync | grep python | awk '{print $2}')
+
+
+synclient MaxTapTime=0 &
+
 nm-applet &
 cinnamon-sound-applet &
 dropbox start
--- .screenlayout/rechtermonitor.sh
+++ .screenlayout/rechtermonitor.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
-xrandr --output VIRTUAL1 --off --output DP2 --off --output DP1 --off --output HDMI2 --off --output HDMI1 --off --output LVDS1 --mode 1366x768 --pos 0x0 --rotate normal --output VGA1 --mode 1280x1024 --pos 1366x0 --rotate normal
+xrandr\
+ --output VIRTUAL1 --off\
+ --output DP2 --off\
+ --output DP1 --off\
+ --output HDMI2 --off\
+ --output HDMI1 --off
+xrandr\
+ --output LVDS1 --auto\
+ --output VGA1 --auto --right-of LVDS1
