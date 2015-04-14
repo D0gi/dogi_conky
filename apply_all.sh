f728d1903ba5e5873d562c405d747024dac7df25
Philipp Dockhorn <philipp.dockhorn@gmail.com>
2015-04-14 18:18:28 +0200

    ...

--- apply_all.sh
+++ apply_all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-cp -ru ./.config ~/
-cp -ru ./.conkyskripte ~/
-cp -ru ./.screenlayout ~/
+cp -r ./.config ~/
+cp -r ./.conkyskripte ~/
+cp -r ./.screenlayout ~/
 cp -u .conkyrc ~/
 cp -u .xsession ~/
 sudo cp -u theme.lua /usr/share/awesome/themes/default
